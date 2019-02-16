d6fa7b417869f0429ba5b101df69523c275164c1
Eric Canam <eric.canam@live.ca>
2019-02-16 17:26:39 -0500

    Get Ad progress
    And some additional tings in universal functions

--- REST/get_ad.php
+++ REST/get_ad.php
@@ -1,5 +1,21 @@
 <?php
 	include "./universal.php";
 	
+	if(!isset($_GET["id"])){
+		writeError("Must specify an ad ID.");
+		exit;
+	}else if(!is_positive_integer($_GET["id"])){
+		writeError("Ad ID not valid.");
+		exit;
+	}
+	
 	$conn = make_connection();
+	
+	$q1 = query($conn, "SELECT * FROM ads WHERE ads.aid=".$_GET["id"]);
+	if($q1===false){
+		writeError("Ad with ID ".$_GET["id"]." not found.");
+		exit;
+	}
+	
+	var_dump($q1);
 ?>
--- REST/universal.php
+++ REST/universal.php
@@ -1,15 +1,20 @@
 <?php
 	function writeError($text) {
-		
+		echo $text."<br />";
+	}
+	
+	function is_positive_integer($str) {
+		return (is_numeric($str) && $str > 0 && $str == round($str));
 	}
 	
 	function make_connection() {
 		$servername = "104.154.234.91";
 		$username = "root";
 		$password = "wornhub1";
+		$db = "wornhub";
 
 		// Create connection
-		$conn = new mysqli($servername, $username, $password);
+		$conn = new mysqli($servername, $username, $password, $db);
 
 		// Check connection
 		if ($conn->connect_error) {
@@ -18,5 +23,14 @@
 		} 
 		
 		return $conn;
+	}
+	
+	function query($conn, $query){
+		$r = $conn->query($query);
+		if($r->num_rows > 1){
+			return $r->fetch_assoc();
+		}else{
+			return false;
+		}
 	}
 ?>
