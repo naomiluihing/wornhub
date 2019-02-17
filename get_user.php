93edc17f0dba690a8ea0a58963b37a01284aa9f4
Naomi Lui-HIng <naomiluihing@cmail.carleton.ca>
2019-02-16 23:49:42 -0500

    Create get_user.php

---
+++ get_user.php
@@ -0,0 +1,25 @@
+<?php
+	include "./universal.php";
+	
+	if(!isset($_GET["id"])){
+		writeError("Must specify a user ID.");
+		exit;
+	}else if(!is_positive_integer($_GET["id"])){
+		writeError("User ID not valid.");
+		exit;
+	}
+	
+	$conn = make_connection();
+	
+	$q1 = query($conn, "SELECT * FROM users WHERE users.uid=".$_GET["id"]);
+	if($q1===false){
+		writeError("User with ID ".$_GET["id"]." not found.");
+		exit;
+	}
+	
+		
+	echo "{'".$q1["uid"]."':{'name':'".$q1["name"]."',
+	'email':'".$q1["email"]."',
+	'phone number':'".$q2["phone"]."',
+	'postal':'".$q2["postal"]."'}}";
+?>
